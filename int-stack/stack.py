7e8d8e67ac32ef24e6e40db95d4d40ad4366a37b
ftnext <takuyafjp+develop@gmail.com>
2022-08-31 22:21:56 +0900

    [feat] 複数の値を覚えておくように実装 (GREEN)

--- int-stack/stack.py
+++ int-stack/stack.py
@@ -1,19 +1,20 @@
 class Stack:
     def __init__(self) -> None:
         self.size = 0
+        self.elements: list[int] = []
 
     def is_empty(self) -> bool:
         return self.size == 0
 
     def push(self, element: int) -> None:
         self.size += 1
-        self.element = element
+        self.elements.append(element)
 
     def pop(self) -> int:
         if self.size == 0:
             raise Underflow()
         self.size -= 1
-        return self.element
+        return self.elements.pop(self.size)
 
     def get_size(self) -> int:
         return self.size
